298a9657804858c5216f059308ce4b8a34df6ab4
Jonathan Li <jonli@secondspectrum.com>
2017-01-21 23:39:49 -0800

    added manhattan distance heuristic

--- Heuristic.java
+++ Heuristic.java
@@ -1,5 +1,7 @@
+import java.lang.Math;
+
 public class Heuristic {
-	public int misplacedTiles(Board board) {
+	public int misplacedTile(Board board) {
 		int count = 0;
 		String[] boardState = new String[9];
 		boardState[0] = board.topLeft.getValue();
@@ -19,6 +21,25 @@ public class Heuristic {
 
 	public int manhattanDist(Board board) {
 		int count = 0;
+		String[] boardState = new String[9];
+		boardState[0] = board.topLeft.getValue();
+		boardState[1] = board.topMid.getValue();
+		boardState[2] = board.topRight.getValue();
+		boardState[3] = board.midLeft.getValue();
+		boardState[4] = board.midMid.getValue();
+		boardState[5] = board.midRight.getValue();
+		boardState[6] = board.botLeft.getValue();
+		boardState[7] = board.botMid.getValue();
+		boardState[8] = board.botRight.getValue();
+    for (int i = 0; i < boardState.length; i++) {
+      if (!boardState[i].equals("0")) {
+        int num = Integer.parseInt(boardState[i]);
+        int vert = Math.abs((num / 3) - (i / 3));
+        int horz = Math.abs((num % 3) - (i % 3));
+        count += vert;
+        count += horz;
+      }
+    }
 		return count;
 	}
 }
--- Node.java
+++ Node.java
@@ -1,9 +1,10 @@
 public class Node {
-	private String value;
+	private String value, initValue;
 	private Node top, left, right, bot;
 
 	public void initialize(int val, Node top, Node left, Node right, Node bot) {
 		this.value = Integer.toString(val);
+		this.initValue = Integer.toString(val);
 		this.top = top;
 		this.left = left;
 		this.right = right;
@@ -19,6 +20,10 @@ public class Node {
 	public String getValue() {
 		return this.value;
 	}
+
+  public String getInitValue() {
+    return this.initValue;
+  }
 
 	public Node getTop() {
 		return this.top;
